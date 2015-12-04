a455d28c2c7aed92072b83d834e7dcdbae336ce2
kevincxm <kevincxm@gmail.com>
2015-12-03 23:26:27 -0600

    ip changed

--- SpaceBookInc/src/Node.java
+++ SpaceBookInc/src/Node.java
@@ -35,7 +35,7 @@ public class Node
 	public static Logger _logger = Logger.getLogger(Node.class);
 	public final static int _portSender = 2001;
 	public final static int _portReceiver = 2000;
-	public static String _introducerIp = "130.126.28.9";
+	public static String _introducerIp = "192.17.11.91";
 	public static boolean _listenerThreadStop = false;
 	public static String _machineIp = "";
 	public static String _machineId= "";
